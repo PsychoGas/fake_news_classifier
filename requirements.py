b2a31c1706ba33d7d0988f32881b1b74c7352cee
Psycho <psychogas69@gmail.com>
2024-05-27 15:51:39 +0530

    make requirements check file to ensure all imports are working

---
+++ requirements.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import itertools
+import sklearn as sk
+
+
+print("Numpy: ", np.__version__)
+print("Pandas: ", pd.__version__)
+print("SKlearn: ", sk.__version__)
+print("Check for itertools: ", itertools.product("ABCD", repeat=2))
